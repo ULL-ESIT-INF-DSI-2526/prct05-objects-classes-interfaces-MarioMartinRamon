e32f17f1246b757ac549587ce9d5f514e54375c0
MarioMartinRamon <alu0101641379@ull.edu.es>
2026-03-04 21:41:17 +0000

    Test: Ej-2 Verificar ganador

--- tests/ejercicio-2.spec.ts
+++ tests/ejercicio-2.spec.ts
@@ -9,4 +9,75 @@ describe('Ejercicio 2', () => {
     expect(juego.colocarFicha(0, 'R')).toBe(true);
     expect(juego.colocarFicha(0, 'A')).toBe(true);
   });
+
+    test('Colocar ficha en columna llena', () => {
+    const j1 = new Player('Jugador 1', 'R');
+    const j2 = new Player('Jugador 2', 'A');
+    const juego = new Juego(j1, j2);
+    for (let i = 0; i < 6; i++) {
+      expect(juego.colocarFicha(0, 'R')).toBe(true);
+    }
+    expect(juego.colocarFicha(0, 'A')).toBe(false);
+  });
+
+    test('Verificar ganador horizontal', () => {
+    const j1 = new Player('Jugador 1', 'R');
+    const j2 = new Player('Jugador 2', 'A');
+    const juego = new Juego(j1, j2);
+    for (let col = 0; col < 4; col++) {
+      expect(juego.colocarFicha(col, 'R')).toBe(true);
+    }
+    expect(juego.verificarGanador('R')).toBe(true);
+  });
+
+  test('Verificar ganador vertical', () => {
+    const j1 = new Player('Jugador 1', 'R');
+    const j2 = new Player('Jugador 2', 'A');
+    const juego = new Juego(j1, j2);
+    for (let i = 0; i < 4; i++) {
+      expect(juego.colocarFicha(0, 'R')).toBe(true);
+    }
+    expect(juego.verificarGanador('R')).toBe(true);
+  });
+
+  test('Verificar ganador diagonal abajo derecha', () => {
+    const j1 = new Player('Jugador 1', 'R');
+    const j2 = new Player('Jugador 2', 'A');
+    const juego = new Juego(j1, j2);
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < i; j++) {
+        expect(juego.colocarFicha(i, 'A')).toBe(true);
+      }
+      expect(juego.colocarFicha(i, 'R')).toBe(true);
+    }
+    expect(juego.verificarGanador('R')).toBe(true);
+  });
+
+  test('Verificar ganador diagonal arriba derecha', () => {
+    const j1 = new Player('Jugador 1', 'R');
+    const j2 = new Player('Jugador 2', 'A');
+    const juego = new Juego(j1, j2);
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < i; j++) {
+        expect(juego.colocarFicha(i, 'A')).toBe(true);
+      }
+      expect(juego.colocarFicha(i, 'R')).toBe(true);
+    }
+    expect(juego.verificarGanador('R')).toBe(true);
+  });
+
+  test('Verificar empate', () => {
+    const j1 = new Player('Jugador 1', 'R');
+    const j2 = new Player('Jugador 2', 'A');
+    const juego = new Juego(j1, j2);
+
+    for (let col = 0; col < 7; col++) {
+      for (let row = 0; row < 6; row++) {
+        const ficha = (row + col) % 2 === 0 ? 'R' : 'A';
+        expect(juego.colocarFicha(col, ficha)).toBe(true);
+      }
+    }
+    expect(juego.verificarGanador('R')).toBe(false);
+    expect(juego.verificarGanador('A')).toBe(false);
+  });
 });
